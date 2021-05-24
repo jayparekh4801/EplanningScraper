1a88f24469b52548619d48e16327787531f62925
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-24 08:24:21 +0530

    initial commit

---
+++ Eplanner/spiders/eplannig.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class EplannigSpider(scrapy.Spider):
+    name = 'eplanning'
+    allowed_domains = ['eplanning.ie']
+    start_urls = ['http://eplanning.ie/']
+
+    def parse(self, response):
+        urls = response.xpath('//a[@target = "_blank"]/@href').getall()
+
+        for url in urls :
+            yield response.follow(url = url, callback = self.parse_url)
+    
+    def parse_url(self, response) :
+        url = response.xpath('//span[contains(@class, "glyphicon glyphicon-inbox btn-lg")]/following-sibling::a/@href').get()
+        yield response.follow(url = url, callback = self.parse_form)
+
+    def parse_form(self, response) :
+        pass
