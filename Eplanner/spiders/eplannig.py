6c1c490221716cfd85cc0f67807dd3b960fc90aa
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-26 07:11:38 +0530

    table scraped

--- Eplanner/spiders/eplannig.py
+++ Eplanner/spiders/eplannig.py
@@ -28,4 +28,26 @@ class EplannigSpider(scrapy.Spider):
         
     
     def parse_pages(self, response) :
-        print(response.body)
+        next_page = response.xpath('//ul[@class = "pagination"]/li[@class = "active"]/following-sibling::li[1]')
+        table = response.xpath('//table')
+        rows = table.xpath('.//tr')
+
+        for ind, row in enumerate(rows) :
+            if(ind == 0) :
+                pass
+
+            yield {
+                "link" : row.xpath('./td[1]/a/@href').get(),
+                "app_status" : row.xpath('./td[2]/text()').get(),
+                "due_date" : row.xpath('./td[3]/text()').get(),
+                "date" : row.xpath('./td[4]/text()').get(),
+                "decidsion_code" : row.xpath('./td[5]/text()').get(),
+                "received_date" : row.xpath('./td[6]/text()').get(),
+                "app_name" : row.xpath('./td[7]/text()').get(),
+                "dev_add" : row.xpath('./td[8]/text()').getall(),
+                "dev_desc" : row.xpath('./td[9]/text()').get(),
+                "lo_auth_name" : row.xpath('./td[10]/text()').get()
+            }
+        
+        yield response.follow(url = next_page, callback = self.parse_pages)
+        # yield scrapy.Request(url=next_page, callback=self.parse_pages)
