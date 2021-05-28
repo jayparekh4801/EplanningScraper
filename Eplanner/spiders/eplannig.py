77f8b43dfcea54f9a90ba19f4c2c6441a9e61d9a
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-28 06:40:28 +0530

    agent status found

--- Eplanner/spiders/eplannig.py
+++ Eplanner/spiders/eplannig.py
@@ -35,21 +35,32 @@ class EplannigSpider(scrapy.Spider):
 
         for ind, row in enumerate(rows) :
             if(ind == 0) :
-                pass
-
-            yield {
-                "link" : row.xpath('./td[1]/a/@href').get(),
-                "app_status" : row.xpath('./td[2]/text()').get(),
-                "due_date" : row.xpath('./td[3]/text()').get(),
-                "date" : row.xpath('./td[4]/text()').get(),
-                "decidsion_code" : row.xpath('./td[5]/text()').get(),
-                "received_date" : row.xpath('./td[6]/text()').get(),
-                "app_name" : row.xpath('./td[7]/text()').get(),
-                "dev_add" : row.xpath('./td[8]/text()').getall(),
-                "dev_desc" : row.xpath('./td[9]/text()').get(),
-                "lo_auth_name" : row.xpath('./td[10]/text()').get()
-            }
+                continue
+                
+            application_url = row.xpath('./td[1]/a/@href').get()
+            application_url = response.urljoin(application_url)
+            yield scrapy.Request(url=application_url, callback=self.parse_agent)
+            # for scraping table
+            # yield {
+            #     "link" : row.xpath('./td[1]/a/@href').get(),
+            #     "app_status" : row.xpath('./td[2]/text()').get(),
+            #     "due_date" : row.xpath('./td[3]/text()').get(),
+            #     "date" : row.xpath('./td[4]/text()').get(),
+            #     "decidsion_code" : row.xpath('./td[5]/text()').get(),
+            #     "received_date" : row.xpath('./td[6]/text()').get(),
+            #     "app_name" : row.xpath('./td[7]/text()').get(),
+            #     "dev_add" : row.xpath('./td[8]/text()').getall(),
+            #     "dev_desc" : row.xpath('./td[9]/text()').get(),
+            #     "lo_auth_name" : row.xpath('./td[10]/text()').get()
+            # }
         # print(next_page)
         # both works
-        # yield scrapy.Request(next_page, callback = self.parse_pages)
-        yield response.follow(url=next_page, callback=self.parse_pages)
+        yield scrapy.Request(next_page, callback = self.parse_pages)
+        # yield response.follow(url=next_page, callback=self.parse_pages)
+    
+    def parse_agent(self, response) :
+        agent = response.xpath('//input[@title = "Show Agents Popup"]/@style').get()
+        if(agent == "display: inline;  visibility: visible;") :
+            pass
+        else : 
+            self.logger.info("button not present -----------------")
